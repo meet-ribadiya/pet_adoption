548643c874830aa367920a494cdf5df599db0b74
meet-crawl <meetribadiya.crawlapps@gmail.com>
2026-03-18 23:54:25 +0530

    fix: render port issue

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,6 @@ async function bootstrap() {
   app.use(compression());
   app.use(morgan('combined'));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
